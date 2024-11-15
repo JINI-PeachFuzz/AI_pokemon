2dd39308cbf45284f3811bb58d0c5e6ada423c3c
JINJU PARK <aricclunj@naver.com>
2024-11-15 17:45:25 +0900

    11.15 로그인,회원가입페이지 틀만들기

---
+++ src/main/java/org/koreait/member/controllers/RequestJoin.java
@@ -0,0 +1,43 @@
+package org.koreait.member.controllers;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.koreait.member.constants.Gender;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+public class RequestJoin extends RequestAgree{
+
+    @Email
+    @NotBlank
+    private String email; // 이메일
+
+    @NotBlank
+    private String name; // 회원명
+
+    @NotBlank
+    private String password; // 비밀번호
+
+    @NotBlank
+    private String confirmPassword; // 비밀번호 확인
+
+    @NotBlank
+    private String nickName; // 닉네임
+
+    @NotNull
+    private LocalDate birthDt;  // 생년월일
+
+    @NotNull
+    private Gender gender; // 성별
+
+    @NotBlank
+    private String zipCode; // 우편번호
+
+    @NotBlank
+    private String address; // 주소
+    private String addressSub; // 나머지 주소
+}
