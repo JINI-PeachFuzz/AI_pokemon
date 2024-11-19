d7d382d09cfd132d3821b265cfefd726a79b953a
JINJU PARK <aricclunj@naver.com>
2024-11-19 18:40:11 +0900

    11.19 약관, 주소 초기셋

--- src/main/java/org/koreait/global/libs/Utils.java
+++ src/main/java/org/koreait/global/libs/Utils.java
@@ -2,13 +2,17 @@ package org.koreait.global.libs;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
+
+import java.util.Locale;
 
 @Component
 @RequiredArgsConstructor
 public class Utils {
 
     private final HttpServletRequest request;
+    private final MessageSource messageSource;
 
     public boolean isMobile(){
 
@@ -26,5 +30,11 @@ public class Utils {
 
         return String.format("%s/%s", prefix, path);
 
+    }
+    // 메세지 코드로 조회된 문구
+    public String getMessage(String code) {
+        Locale lo = request.getLocale(); // 사용자 요청 헤더(Accept-Language)
+
+        return messageSource.getMessage(code, null, lo);
     }
 }
