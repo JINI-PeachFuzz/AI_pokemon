da4bf5e1704506515bfa2feb3d4c0afced9bb147
JINJU PARK <aricclunj@naver.com>
2024-11-25 01:57:03 +0900

    11/24 home

--- src/main/java/org/koreait/global/configs/MvcConfig.java
+++ src/main/java/org/koreait/global/configs/MvcConfig.java
@@ -6,6 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+    /**
+     * 정적 경로 설정, CSS, JS, 이미지
+     *
+     * @param registry
+     */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
