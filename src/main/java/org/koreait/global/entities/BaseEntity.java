19c1205d728dd194b965b059db450627ca627152
JINJU PARK <aricclunj@naver.com>
2024-11-28 10:19:08 +0900

    11.28 환경변수 설정 DB연결

---
+++ src/main/java/org/koreait/global/entities/BaseEntity.java
@@ -0,0 +1,30 @@
+package org.koreait.global.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class) // 사용할때마다 설정하게 새로고침느낌으로 MvcConfig에 추가함
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt; // 등록일시
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime modifiedAt; // 수정일시
+
+
+    private LocalDateTime deletedAt; // 삭제일시
+}
--- src/main/java/org/koreait/pokemon/entities/Pokemon.java
+++ src/main/java/org/koreait/pokemon/entities/Pokemon.java
@@ -1,17 +1,31 @@
 package org.koreait.pokemon.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import lombok.Data;
+import org.koreait.global.entities.BaseEntity;
+
 // db에 넣을 목록들
 @Data
-
-public class Pokemon {
+@Entity
+public class Pokemon extends BaseEntity {
+    @Id
     private Long seq;
+
+    @Column(length = 50)
     private String name; // 포켓몬 한글 이름
+
+    @Column(length = 50)
     private String nameEn; // 포켓몬 영어 이름
     private int weight;
     private int height;
     private int baseExperience;
+
     private String frontImage;
+
+    @Lob
     private String flavorText; // 설명
     private String types; // 타입1||타입2||타입3 으로 가공할 예정
     private String abilities; // 능력1||능력2||능력3
