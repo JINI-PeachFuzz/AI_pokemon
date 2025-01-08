ec98a7e25f45029be12e1e286151e63a9916ef58
JINJU PARK <aricclunj@naver.com>
2025-01-08 18:23:17 +0900

    01.08 쇼핑몰 이미지첨부 추가

--- src/main/java/org/koreait/admin/product/controllers/ProductController.java
+++ src/main/java/org/koreait/admin/product/controllers/ProductController.java
@@ -7,6 +7,7 @@ import org.koreait.file.constants.FileStatus;
 import org.koreait.file.services.FileInfoService;
 import org.koreait.global.annotations.ApplyErrorPage;
 import org.koreait.global.libs.Utils;
+import org.koreait.product.constants.DiscountType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -59,6 +60,7 @@ public class ProductController implements SubMenus {
         commonProcess("add", model); // save를 해서 상품등록 수정을 같이
 
         form.setGid(UUID.randomUUID().toString()); // 중복되지않는 ID생성
+        form.setDiscountType(DiscountType.NONE); // 기본값설정
 
         return "admin/product/add";
     }
